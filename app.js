c42408991e7ca8f0f5b64fd3b5c2d0893b220287
Harry Clemente <hclemente1989@gmail.com>
2020-10-19 17:02:27 -0700

    Completed React Components check boxes

--- app.js
+++ app.js
@@ -8,19 +8,37 @@ var App = () => (
 
 var GroceryList = (props) => (
 <ul>
-  {props.items.map((currentElement) => (
+  {/* {props.items.map((currentElement) => (
     <GroceryListItem item={currentElement}/>
-  ))}
+  ))} */}
+  <GroceryListItem item={props.items[0]}/>
+  <GroceryListItem item={props.items[1]}/>
+  <GroceryListItem item={props.items[2]}/>
 </ul>
 );
 
-var GroceryListItem = (props) => (
-    <ul>
-          <li>{props.item}</li>
-    </ul>
-);
+class GroceryListItem extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      done: false
+    };
+  }
 
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
 
+  render () {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (<li style={style} onMouseOver={this.onListItemClick.bind(this)}>{this.props.item}</li>);
+  };
+}
 
 ReactDOM.render(<App />, document.getElementById("app"));
 
